43d76dd069a2c75b7f7efd06a133c90134bd7e88
yuriy_beloff <paganismrus@gmail.com>
2018-04-07 14:08:42 +0300

    used unpacking in image.size

--- README.md
+++ README.md
@@ -1,14 +1,27 @@
 # Image Resizer
 
-Module takes path to image and resizes it according to your arguments and saves it to the same directory
+Module takes path to image and resizes it according to your arguments and saves it to the same directory or to the directory which could be set
 # Usage Example
 ```
 $ python3.5 image_size.py --path /home/user/images/img.jpg --scale 2
-```
-optional parameters:
---width
---height
 
+Required parameter:
+--path
+  required parameter that specifies path to source image
+  (ex. --path /tmp/source_image.jpg)
+
+Optional parameters:
+--width
+  parameter specifies the width of the new image (ex. --width 1280)
+--height
+  parameter specifies the height of the new image (ex. --height 720)
+--scale
+  parameter specifies the scale of the new image (ex. --scale 2)
+--dest
+  parameter specifies the dest directory of the new image (ex. --dest /tmp/)
+
+Parameters  --width and --height can't be uset with parameter --scale and vise versa
+```
 # Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- image_resize.py
+++ image_resize.py
@@ -10,50 +10,60 @@ def get_image(path_to_image):
 
 
 def resize_image(image, width, height, scale):
-    old_width = image.size[0]
-    old_height = image.size[1]
+    old_width, old_height = image.size
     ratio = old_width / old_height
     if not height and width:
-        resized_image = image.resize((int(width),
-                                      int(int(width) / ratio)),
-                                     Image.ANTIALIAS)
+        new_height = int(width / ratio)
+        new_width = width
+    if not width and height:
+        new_width = int(height * ratio)
+        new_height = height
     if height and width:
-        resized_image = image.resize((int(width),
-                                      int(height)),
-                                     Image.ANTIALIAS)
-    if scale and not all([scale, width]):
-        resized_image = image.resize((int(old_width * scale),
-                                      int(old_height * scale)),
-                                     Image.ANTIALIAS)
+        new_width = width
+        new_height = height
+        if ratio != new_width/new_height:
+            print('Соотношение сторон изменено')
+    if scale:
+        new_width = int(old_width * scale)
+        new_height = int(old_height * scale)
+    resized_image = image.resize((new_width, new_height), Image.ANTIALIAS)
     return resized_image
 
 
-def save_resized_image(resized_image):
-    width = resized_image.size[0]
-    height = resized_image.size[1]
-    resized_image.save('pic__{}x{}.jpg'.format(width, height))
+def save_resized_image(resized_image, source_name, source_format, image_dir):
+    width, height = resized_image.size
+    new_image_name = '{}__{}x{}.{}'.format(source_name,
+                                           width, height, source_format)
+    resized_image.save(os.path.join(image_dir, new_image_name))
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--path', dest='path', type=str,
-                        required=True, help='Path to image file')
+    parser.add_argument('--path', dest='path', required=True,
+                        help='Path to image file')
     parser.add_argument('--width', dest='width', type=int,
                         required=False, default=None)
     parser.add_argument('--height', dest='height', type=int,
                         required=False, default=None)
     parser.add_argument('--scale', dest='scale', type=float,
                         required=False, default=None)
+    parser.add_argument('--dest', dest='dest',
+                        required=False, default=None)
     return parser.parse_args()
 
 
 if __name__ == '__main__':
     args = parse_arguments()
-    if args.scale and (args.width or args.height) \
-            or (args.height and not args.width):
+    if args.scale and (args.width or args.height):
         sys.exit('Некорректно указаны аргументы')
     if not os.path.exists(args.path):
         sys.exit('Файл по указанному пути не существует')
     image = get_image(args.path)
+    source_name, ext = os.path.basename(args.path).split('.')
+    if not args.dest:
+        image_dir = os.path.dirname(args.path)
+    else:
+        image_dir = args.dest
+    source_format = image.format.lower()
     resized_image = resize_image(image, args.width, args.height, args.scale)
-    save_resized_image(resized_image)
+    save_resized_image(resized_image, source_name, ext, image_dir)
